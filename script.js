7a671e2711d7265211fc6469aec851a688cf08fb
mhpetersen <mathiaspetersen98@gmail.com>
2020-11-11 07:59:44 +0100

    Fra Tirsdag

---
+++ script.js
@@ -0,0 +1,20 @@
+
+$( document ).ready(function() {
+     
+});
+
+function UpdateIntroduction()
+{
+    var titles = [ "student", "programmer", "developer", "memer and suicide is badass", "machine" ];
+    
+    // Define our counter as a number
+    if(typeof UpdateIntroduction.counter == 'undefined' ) {
+        UpdateIntroduction.counter = 0;
+    }
+
+    document.getElementById("content-welcome-text").innerText= "i'm a " + titles[UpdateIntroduction.counter % titles.length];
+    
+    UpdateIntroduction.counter++;
+
+    setTimeout(UpdateIntroduction, 3000);
+}
