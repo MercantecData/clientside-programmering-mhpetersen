f006351ae7e5575a2ebab47cb93144b9cc25264d
mhpetersen <mathiaspetersen98@gmail.com>
2020-11-12 18:55:27 +0100

    Torsdags updates

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 
-$( document ).ready(function() {
-     
-});
-
 function UpdateIntroduction()
 {
-    var titles = [ "student", "programmer", "developer", "memer", "machine" ];
+    var titles = [ "student", "programmer", "developer", "server admin", "machine" ];
     
-    // Define our counter as a number
-    if(typeof UpdateIntroduction.counter == 'undefined' ) {
+    if(typeof UpdateIntroduction.counter == "undefined" ) {
         UpdateIntroduction.counter = 0;
     }
 
     document.getElementById("content-welcome-text").innerText= "i'm a " + titles[UpdateIntroduction.counter % titles.length];
-    
+
     UpdateIntroduction.counter++;
 
     setTimeout(UpdateIntroduction, 3000);
+}
+
+function LoadInBio()
+{
+    var paragrams = 1;
+
+    if(typeof LoadInBio.loadInStage == "undefined" ) {
+        LoadInBio.loadInStage = 0;
+    }
+
+    if(LoadInBio.loadInStage >= paragrams)
+        return;
+
+        LoadInBio.loadInStage++;
+
+    var elements = document.getElementsByClassName("content-left-bio");
+    for(int i = 0; i<LoadInBio.loadInStage ;i++)
+    {
+        elements[i]
+    }
+
+    setTimeout(LoadInBio, 3000);
 }
